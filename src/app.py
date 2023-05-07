cc0260393ec0232a91090c0fb160ca587e86d202
Wei Deng <dw-dengwei@outlook.com>
2023-05-07 10:47:00 +0800

    动态返回

--- src/app.py
+++ src/app.py
@@ -1,16 +1,112 @@
-from flask import Flask
+from flask import Flask, Response, request
 from flask_cors import CORS
 import json
+import numpy as np
+from PIL import Image
+from io import BytesIO
+from torchvision import transforms
+
 
 app = Flask(__name__) 
 CORS(app, supports_credentials=True)
 
-@app.post('/get_model')
-def hello():
-    with open('res/model.json', 'r') as f:
-        a = json.loads(f.read())
-    return a
 
+def gen_tensor(shape):
+    return np.random.rand(*shape).tolist()
+
+
+@app.post('/get_model')
+def get_model():
+    layers = [
+        {
+            'name': 'conv_1_1',
+            'kernel': gen_tensor((10, 3, 3, 3)),
+            'bias': gen_tensor((10,))
+        },
+        {
+            'name': 'relu_1_2',
+        },
+
+        {
+            'name': 'conv_2_1',
+            'kernel': gen_tensor((10, 10, 3, 3)),
+            'bias': gen_tensor((10,))
+        },
+        {
+            'name': 'relu_2_2',
+        },
+        {
+            'name': 'pool_2_3',
+        },
+
+        {
+            'name': 'conv_3_1',
+            'kernel': gen_tensor((10, 3, 3, 3)),
+            'bias': gen_tensor((10,))
+        },
+        {
+            'name': 'relu_3_2',
+        },
+
+        {
+            'name': 'conv_4_1',
+            'kernel': gen_tensor((10, 10, 3, 3)),
+            'bias': gen_tensor((10,))
+        },
+        {
+            'name': 'relu_4_2',
+        },
+        {
+            'name': 'pool_4_3',
+        },
+
+        {
+            'name': 'flatten_5_1',
+        },
+
+        {
+            'name': 'fc_6_1',
+            'kernel': gen_tensor((10, 1690)),
+            'bias': gen_tensor((10,))
+        },
+    ]
+    jsonModel = {'layers': layers}
+    return json.dumps(jsonModel)
+    
+
+@app.post('/get_feature_map')
+def get_feature_map():
+    img_bin = request.files.get('file').stream.read()
+    img_np = np.array(Image.open(BytesIO(img_bin)))
+    img_tensor = transforms.ToTensor()(img_np)
+   
+    # model(img_tensor) 
+    
+    allOutputs = [
+        (10, 62, 62),
+        (10, 62, 62),
+        (10, 60, 60),
+        (10, 60, 60),
+        (10, 30, 30),
+        (10, 28, 28),
+        (10, 28, 28),
+        (10, 26, 26),
+        (10, 26, 26),
+        (10, 13, 13),
+        (1690,),
+        (10,)
+    ]
+
+    inputImageArray = img_tensor.cpu().numpy().tolist()
+    allOutputs = [gen_tensor(shape) for shape in allOutputs]
+
+    res = {
+        'allOutputs': allOutputs,
+        'inputImageArray': inputImageArray
+    }
+
+    return json.dumps(res)
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000) 
